877b4a2e2114dc7f6d4abca184125d5264ac3799
Fankillerman <nice.sasha.03@gmail.com>
2025-12-22 00:07:37 +0100

    3.5 sumRows3 + toTwoArgs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
-module Lib (isSorted) where
+module Lib (isSorted, toTwoArgs,sumRows3) where
 
 isSorted :: (a -> a -> Bool) -> [a] -> Bool
 isSorted _ [] = True
 isSorted _ [_] = True
 isSorted cmp (x:y:rest) = cmp x y && isSorted cmp (y:rest)
+
+
+toTwoArgs :: (a -> b -> c -> d) -> a -> (b, c) -> d
+toTwoArgs f a (b, c) = f a b c
+
+
+sumRows3 :: Num n => [n] -> [n] -> [n] -> [n]
+sumRows3 xs ys zs =
+  zipWith (toTwoArgs (\x y z -> x + y + z)) xs (zip ys zs)
